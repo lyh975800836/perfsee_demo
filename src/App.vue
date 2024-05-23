5397494b55e0b0d91f5ee3561a03a95bf0096d8f
liyuheng <http://git.baijia.com/liyuheng>
2024-05-23 16:47:17 +0800

    feat: test

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <div>demo111</div>
+    <div>demo111222</div>
   </div>
 </template>
 
