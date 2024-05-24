0aaee985b30303824d05fc3fe398d54bca4585fe
liyuheng <http://git.baijia.com/liyuheng>
2024-05-24 19:42:38 +0800

    feat: l

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
         // ...
         new PerfseePlugin({
           project: 'perfsee-demo',
-          platform: 'http://127.0.0.1:8080'
+          platform: 'http://127.0.0.1:3000'
         //   enableAudit: true,
         })
     ]
